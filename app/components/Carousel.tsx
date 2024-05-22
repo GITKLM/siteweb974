2caa973795b8217c374d2ae1b1f8a908af4f73a8
GITKLM <cprioton@gmail.com>
2024-05-22 17:47:07 +0400

    test img size Caroussel

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
-
 const Carousel = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const totalSlides = 5;
@@ -23,22 +22,46 @@ const Carousel = () => {
     setCurrentSlide((prevSlide) => (prevSlide - 1 + totalSlides) % totalSlides);
   };
 
+  // Hook pour obtenir la taille de la fenêtre
+  const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState({
+      width: typeof window !== 'undefined' ? window.innerWidth : 0,
+      height: typeof window !== 'undefined' ? window.innerHeight : 0,
+    });
+
+    useEffect(() => {
+      function handleResize() {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowSize;
+  };
+
+  const windowSize = useWindowSize();
+
   useEffect(() => {
     const interval = setInterval(goToNextSlide, 20000);
 
     return () => clearInterval(interval);
   }, [currentSlide]);
+
   return (
     <div className="carousel">
-        <Image
+      <Image
         className='rounded-lg'
-          src={slides[currentSlide]} 
-          alt={`Projet n°${currentSlide + 1}`}
-          height={555} 
-          width={1100} 
-        />
-      </div>
-
+        src={slides[currentSlide]} 
+        alt={`Projet n°${currentSlide + 1}`}
+        width={windowSize.width < 768 ? windowSize.width : 1100} 
+        height={windowSize.width < 768 ? windowSize.width * 0.5 : 555} 
+      />
+    </div>
   );
 };
 
