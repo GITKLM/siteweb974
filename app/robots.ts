88af2f59cb4a8abaa1bb40253718a09d8d414af4
GITKLM <cprioton@gmail.com>
2024-05-30 18:47:22 +0400

    robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,14 @@ import { MetadataRoute} from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-        userAgent: '*',
-        allow: '/',
-        disallow: '/pages',
-      },
-      sitemap: 'https://siteweb974.re/sitemap.xml ',
+    rules: [
+    {
+      userAgent: '*',
+      allow: '/',
+      disallow: ["/admin", "/privacy"]
+    },
+    ],
+    sitemap: 'https://siteweb974.re/sitemap.xml ',
+
 }
 }
