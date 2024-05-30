cbc908ab6c265903367fe424522f889bd26d4c93
GITKLM <cprioton@gmail.com>
2024-05-30 13:02:42 +0400

    carousel classique

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -2,37 +2,6 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
-const useWindowSize = () => {
-  const isClient = typeof window === 'object'; // Vérifie si le code s'exécute côté client
-
-  const [windowSize, setWindowSize] = useState({
-    width: isClient ? window.innerWidth : 0,
-    height: isClient ? window.innerHeight : 0,
-  });
-
-  useEffect(() => {
-    if (!isClient) {
-      return; // Si le code s'exécute côté serveur, sortir de l'effet
-    }
-
-    function handleResize() {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-
-    handleResize(); // Mise à jour de la taille de la fenêtre lors du chargement de la page
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, [isClient]); // Déclenche uniquement après le rendu initial côté client
-
-  return windowSize;
-};
-
-     
-
 const Carousel = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const totalSlides = 5;
@@ -53,8 +22,6 @@ const Carousel = () => {
     setCurrentSlide((prevSlide) => (prevSlide - 1 + totalSlides) % totalSlides);
   };
 
-  const windowSize = useWindowSize();
-
   useEffect(() => {
     const interval = setInterval(goToNextSlide, 20000);
 
@@ -63,13 +30,21 @@ const Carousel = () => {
 
   return (
     <div className="carousel">
-      <Image
-        className='rounded-lg'
-        src={slides[currentSlide]} 
-        alt={`Projet n°${currentSlide + 1}`}
-        width={windowSize.width < 768 ? windowSize.width : 1100} 
-        height={windowSize.width < 768 ? windowSize.width * 0.5 : 556} 
-      />
+      <div className="slides-container">
+        {slides.map((slide, index) => (
+          <div key={index} className={`slide ${index === currentSlide ? 'active' : ''}`}>
+            <Image
+              className='rounded-lg'
+              src={slide}
+              alt={`Projet n°${index + 1}`}
+              width={1100}
+              height={556}
+            />
+          </div>
+        ))}
+      </div>
+      <button onClick={goToPrevSlide}>Previous</button>
+      <button onClick={goToNextSlide}>Next</button>
     </div>
   );
 };
